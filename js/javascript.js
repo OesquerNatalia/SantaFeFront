cc4014238ffb9f7e5279a8e8b05619511de253cb
OesquerNatalia <115839681+OesquerNatalia@users.noreply.github.com>
2022-12-06 00:22:56 -0300

    practico js

---
+++ js/javascript.js
@@ -0,0 +1,70 @@
+document.getElementById("nocturno").style.backgroundColor = "blueviolet";
+document.getElementById("nocturno").style.borderColor = "rgb(39, 2, 74)";
+document.getElementById("nocturno").style.color = "black";
+document.getElementById("dia").style.backgroundColor = "blueviolet";
+document.getElementById("dia").style.borderColor = "rgb(39, 2, 74)";
+document.getElementById("dia").style.color = "black";
+
+function dia(){
+    document.body.style.backgroundImage = "none";
+    document.body.style.backgroundColor = "#9999CC"; 
+    document.getElementById("titulo").style.backgroundColor = "black";
+    document.getElementById("titulo").style.color = "blueviolet";
+    document.getElementById("nav").style.backgroundColor ="#180914";
+    document.getElementById("inicio").style.color = "blueviolet";
+    document.getElementById("contacto").style.color = "blueviolet";
+    document.getElementById("mas").style.color = "blueviolet";
+    document.getElementById("receta").style.backgroundColor ="#180914";
+    document.getElementById("receta").style.color = "blueviolet";
+    document.getElementById("texto").style.color = "blue";
+    document.getElementById("ingre").style.backgroundColor ="#180914";
+    document.getElementById("ingre").style.color = "blueviolet";
+    document.getElementById("masa").style.backgroundColor ="#180914";
+    document.getElementById("masa").style.color = "blueviolet";
+    document.getElementById("masalist").style.color = "blueviolet";
+    document.getElementById("prepa").style.backgroundColor ="#180914";
+    document.getElementById("prepa").style.color = "blueviolet";
+    document.getElementById("prepalist").style.color = "blueviolet";
+    document.getElementById("relleno").style.color = "blueviolet";
+    document.getElementById("relleno").style.backgroundColor ="#180914";
+    document.getElementById("rellenolist").style.color = "blueviolet";
+    document.getElementById("otraprepa").style.color = "blueviolet";
+    document.getElementById("otraprepa").style.backgroundColor ="#180914";
+    document.getElementById("otraprepalist").style.color = "blueviolet";  
+}
+
+
+function nocturno(){
+    document.body.style.backgroundImage = "none";
+    document.body.style.backgroundColor = "black";  
+    document.getElementById("nocturno").style.color = "azure"; 
+    document.getElementById("dia").style.color = "azure"; 
+    document.getElementById("titulo").style.color = "white";
+    document.getElementById("inicio").style.color = "white";
+    document.getElementById("contacto").style.color = "white";
+    document.getElementById("mas").style.color = "white";
+    document.getElementById("receta").style.color = "white";
+    document.getElementById("texto").style.color = "white";
+    document.getElementById("ingre").style.color = "white";
+    document.getElementById("masa").style.color = "white";
+    document.getElementById("masalist").style.color = "white";
+    document.getElementById("prepa").style.color = "white";
+    document.getElementById("prepalist").style.color = "white";
+    document.getElementById("relleno").style.color = "white";
+    document.getElementById("rellenolist").style.color = "white";
+    document.getElementById("otraprepa").style.color = "white";
+    document.getElementById("otraprepalist").style.color = "white";   
+    document.getElementById("nav").style.backgroundColor ="#180914";
+    document.getElementById("receta").style.backgroundColor ="#180914";
+    document.getElementById("ingre").style.backgroundColor ="#180914";
+    document.getElementById("masa").style.backgroundColor ="#180914";
+    document.getElementById("prepa").style.backgroundColor ="#180914";
+    document.getElementById("relleno").style.backgroundColor ="#180914";
+    document.getElementById("otraprepa").style.backgroundColor ="#180914";
+    
+}
+
+
+function copyright(){
+    alert("Autora: Natalia Oesquer. Email: nati.oesquer@hotmail.com - nacris83@outlook.com");
+}
